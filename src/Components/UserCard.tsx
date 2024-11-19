7469d06cdce3ff16082f9c39b6e31dd4c522d1af
zaneera <zaneera000@gmail.com>
2024-11-19 16:38:04 +0530

    aignment

--- src/Components/UserCard.tsx
+++ src/Components/UserCard.tsx
@@ -28,9 +28,9 @@ const onChange= ()=>{
                     </div>
                    
                     {/* Buttons Section */}
-                    <div className='flex gap-2 ml-4'>
-                        <button className='bg-slate-900 text-white px-4 py-2 rounded'>Message</button>
-                        <button className='bg-zinc-800 text-white px-4 py-2 rounded'>Following</button>
+                    <div className='flex gap-3 ml-6'>
+                        <button className=' text-white px-4 py-2 rounded border-2 border-slate-600 hover:bg-slate-950 '>Message</button>
+                        <button className=' text-white px-4 py-2 rounded border-2 border-slate-600 hover:bg-slate-950'>Following</button>
                     </div>
                 </div>
 
